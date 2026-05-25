7730961e94864a5b6195659c752ea245b1c7d8f8
ivamly <ivamly.dev@gmail.com>
2026-05-25 17:14:35 +0300

    Initial commit

---
+++ src/test/java/ru/ivamly/migration/MigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ivamly.migration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
